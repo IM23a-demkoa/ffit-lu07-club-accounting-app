fc60e3f43235cc3930d2ce9c76b691289720ed3a
Im23a-demkoa <demkoa@bzz.ch>
2025-10-28 09:50:37 +0100

    Implement JPA mappings for Project, Account, and Booking entities

--- src/main/java/ch/bzz/model/Account.java
+++ src/main/java/ch/bzz/model/Account.java
@@ -1,17 +1,31 @@
 package ch.bzz.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+@Entity
+@Table(name = "account")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
+
+    @Column(name = "accountNumber", nullable = false, unique = true, length = 32)
     private String accountNumber;
+
+    @Column(name = "name", nullable = false, length = 64)
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "projectName", nullable = false)
     private Project project;
 }
--- src/main/java/ch/bzz/model/Booking.java
+++ src/main/java/ch/bzz/model/Booking.java
@@ -1,5 +1,6 @@
 package ch.bzz.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -7,16 +8,37 @@ import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "booking")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
+
+    @Column(name = "date", nullable = false)
     private LocalDate date;
+
+    @Column(name = "text", nullable = false, length = 128)
     private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "debitAccount", nullable = false)
     private Account debitAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "creditAccount", nullable = false)
     private Account creditAccount;
+
+    @Column(name = "amount", nullable = false)
     private double amount;
+
+    @ManyToOne
+    @JoinColumn(name = "projectName", nullable = false)
     private Project project;
 }
